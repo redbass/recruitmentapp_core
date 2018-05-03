e59bc958700e1ea521432d4a4bd9ca9fde1d744c
Luca Gallici <lg@administrates.co>
2018-05-03 18:11:40 +0100

    added advertise location and fixes some integration tests

---
+++ model/location.py
@@ -0,0 +1,28 @@
+import geojson
+from geojson import Point
+
+
+class Location:
+
+    def __init__(self, lng: float, lat: float):
+
+        if lng < -180 or 180 < lng:
+            raise ValueError(
+                'Longitude value have to be -180 <= longitude <= 180')
+
+        if lat < -90 or 90 < lat:
+            raise ValueError(
+                'Latitude value have to be -90 <= latitude <= 90')
+
+        self._point = Point((lng, lat))
+
+    @property
+    def longitude(self):
+        return self._point['coordinates'][0]
+
+    @property
+    def latitude(self):
+        return self._point['coordinates'][1]
+
+    def get_geo_json_point(self):
+        return geojson.dumps(self._point)
--- test_integration/__init__.py
+++ test_integration/__init__.py
@@ -1,5 +1,7 @@
+import json
 from unittest import TestCase
 
+from app import get_app
 from config import settings
 
 
@@ -8,3 +10,10 @@ class IntegrationTestCase(TestCase):
     def setUp(self):
         if not settings.TEST_MODE:
             raise Exception('Test have to run in test mode')
+
+        self.test_app = get_app().test_client()
+
+    def post_json(self, url, data):
+        return self.test_app.post(url,
+                                  data=json.dumps(data),
+                                  content_type='application/json')
