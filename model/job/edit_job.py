c3ccef58769a45873746cef30bd8d172539b98f1
Luca Gallici <lg@administrates.co>
2018-08-31 22:34:44 +0100

    Fix job edit bug (location)

--- model/job/edit_job.py
+++ model/job/edit_job.py
@@ -1,9 +1,11 @@
 from db.collections import jobs
 from db.utils import dict_to_datapath
+from model.job.create_job import _input_location_to_location
 from model.job.job import get_job
 
 
 def edit_job(_id: str,
+             location=None,
              **new_values):
 
     if 'company_id' in new_values:
@@ -12,6 +14,9 @@ def edit_job(_id: str,
     if not get_job(_id):
         raise ValueError('Job id "{job_id}" does not exists'
                          .format(job_id=_id))
+
+    if location:
+        new_values['location'] = _input_location_to_location(location)
 
     exploded_values = dict_to_datapath(new_values)
 
