0edbbe23db3a3a5d638b74987f1a5b7a21174f7e
Luca Gallici <lg@administrates.co>
2018-11-16 18:16:17 +0000

    Fix search by distance

--- lib/geo.py
+++ lib/geo.py
@@ -1,5 +1,4 @@
 EARTH_RADIUS_KM = 6371
-EARTH_RADIUS_MI = 3959
 
 
 def km2rad(km):
--- test/services/search/__init__.py
+++ test/services/search/__init__.py
@@ -1,6 +1,14 @@
+from datetime import datetime, timedelta
+
+from db.collections import _create_text_index, jobs
+from model.job.job_advert import add_advert_to_job, \
+    request_approval_job_advert, approve_job_advert, publish_job_advert
+from test import UnitTestCase
+from test.model.job import JobFactory
+
 EDINBURGH_CENTER = {
     "postcode": "EH3 9DZ", "admin_district": "Edinburgh",
-    "latitude": 55.946998, "longitude": -3.201549
+    "latitude": 55.9544088, "longitude": -3.1700107
 }
 
 EDINBURGH_ZOO = {
@@ -23,7 +31,44 @@ EDINBURGH_ROSELIN_CHAPEL = {
     "latitude": 55.8538158, "longitude": -3.1861218
 }
 
+STERLING_CASTLE = {
+    "postcode": "FK8 1EJ", "admin_district": "Sterling",
+    "latitude": 56.1238708, "longitude": -3.9470077
+}
+
 ITALY = {
     "postcode": "None", "admin_district": "None",
-    "latitude": 46.056057, "longitude": 13.2091431
+    "latitude": 45.8557706, "longitude": 13.056929
 }
+
+
+class BaseSearchTestCase(UnitTestCase):
+
+    def setUp(self):
+        super().setUp()
+        _create_text_index()
+
+    @classmethod
+    def _crate_job(cls, duration=15, expired=False, **job_args):
+        job = cls.create_from_factory(JobFactory, **job_args)
+        job_id = job['_id']
+        advert = add_advert_to_job(job_id=job_id,
+                                   advert_duration_days=duration)
+        advert_id = advert['_id']
+
+        request_approval_job_advert(job_id=job_id, advert_id=advert_id)
+        approve_job_advert(job_id=job_id, advert_id=advert_id)
+        publish_job_advert(job_id=job_id, advert_id=advert_id)
+
+        if expired:
+            cls._make_advert_expired(job_id=job_id, advert_id=advert_id)
+
+        return job
+
+    @classmethod
+    def _make_advert_expired(cls, job_id, advert_id):
+        yesterday = datetime.now() - timedelta(days=1)
+        jobs.update(
+            {'_id': job_id, 'adverts._id': advert_id},
+            {"$set": {'adverts.$.date.expires': yesterday}}
+        )
